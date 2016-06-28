37d3910fb3b7d14aa5b9ffe5002aadfe3a8246ab
jackalwei <jackal8125@gmail.com>
2016-06-29 07:06:15 +0800

    add redirect to short url

--- app/controllers/mappings_controller.rb
+++ app/controllers/mappings_controller.rb
@@ -18,6 +18,22 @@ class MappingsController < ApplicationController
     redirect_to mappings_path
   end
 
+  def regoto
+    if params[:id]
+      url = params[:id]
+
+      Mapping.all.each do |u|
+        if url == u.shotrurl
+          u.connect_counts += 1
+          u.save
+          redirect_to u.longurl
+          return
+        end
+      end
+    end
+    redirect_to mappings_path
+  end
+
   protected
 
   def url_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :mappings
 
+  get '/:id' => 'mappings#regoto'
 
   root :to => "mappings#index"
   # The priority is based upon order of creation: first created -> highest priority.
